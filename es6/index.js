bd373a65d4c85b2f37ce4964ad69acaf0cb61082
snowling444 <937035581@qq.com>
2016-11-12 16:47:58 +0800

    c

---
+++ es6/index.js
@@ -0,0 +1,9 @@
+console.log("boring");
+
+function f(x,y){
+  console.log(x+y)
+}
+f(3,6);
+
+var add = (x,y) => x+y;
+alert(add(5,6));
---
+++ es6/webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'index.js'),//入口
+    output: {
+        // path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+      loaders: [
+        { test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader" 
+        }
+      ]
+    }
+};
